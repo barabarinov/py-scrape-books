fd04fac984e919819f7908fdb8e3d6e636e96f98
barabarinov <barabarinov@gmail.com>
2024-04-22 18:57:15 +0300

    Fixed rating selector

--- books/spiders/book.py
+++ books/spiders/book.py
@@ -22,7 +22,7 @@ class BooksSpider(scrapy.Spider):
             "title": response.css("h1::text").get(),
             "price": response.css(".price_color::text").get(),
             "amount_in_stock": response.css(".instock.availability::text").re_first(r"\d+"),
-            "rating": response.css(".star-rating::attr(class)").re_first(r"Two|Three|Four|Five|One"),
+            "rating": response.css(".star-rating").re_first(r"Two|Three|Four|Five|One"),
             "category": response.css(".breadcrumb li:nth-last-child(2) a::text").get(),
             "description": response.css("#product_description + p::text").get(),
             "upc": response.css('.table-striped th:contains("UPC") + td::text').get(),
